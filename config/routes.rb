91cec08a0e6dde816ca841c2cd5c7510db0ea160
ezekielsilverstein <ezekiel.m.silverstein@gmail.com>
2017-07-13 23:35:20 -0400

    Added users, set players and notes views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :players do
     resources :notes
   end
-  root "players#index"
+  # root "players#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
