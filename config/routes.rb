2bdfabea24a57606127792890d9ce8d3183e383e
ezekielsilverstein <ezekiel.m.silverstein@gmail.com>
2017-07-13 23:37:45 -0400

    Uncomment setting root to players#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :my_teams
   resources :players do
     resources :notes
   end
-  # root "players#index"
+  root "players#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
